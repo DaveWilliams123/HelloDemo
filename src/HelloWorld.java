88e0f6dcdd33205785a6c3f2b7e720b9efb0d4af
DaveWilliams123 <David.g.williams@rbc.com>
2016-07-26 13:46:23 -0400

    nmv nv

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 	}
 public String Hello()
 {
-	if (Math.random() * 101 > 30)
+	if (Math.random() * 101 > 105)
 	{
 		try {
 			Thread.sleep((int) Math.floor(Math.random() * 20000));
